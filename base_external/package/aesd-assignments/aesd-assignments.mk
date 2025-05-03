a0398238c0c8293657113ee2c41b3117f6258c8f
Gineus <41481916+Gineus@users.noreply.github.com>
2025-05-03 18:57:16 +0200

    Use version with corrected parameter

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 20cc2601892e64f558431bd3cf0afe95ce8dd138
+AESD_ASSIGNMENTS_VERSION = 81e4d9e7bca2816fba43ae6ffbdff7c344b79fed
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
